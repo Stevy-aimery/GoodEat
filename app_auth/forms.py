800e08fed9a9ff85e9827b350af9ffd36b00eb00
stevy-kaba <stevyaimery@gmail.com>
2024-01-07 16:36:37 +0100

    adding the the first validation of form logie by django

--- app_auth/forms.py
+++ app_auth/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-class LoginForms(forms.Form):
-    username=forms.CharField(required=True)
+class LoginForm(forms.Form):
+    username=forms.CharField(label="Nom d'utilisateur",widget=forms.TextInput(attrs={'class':'input form-control','placeholder':"Nom d'utilisateur"}),required=True)
     # username=forms.CharField( max_length=50, required=False)
-    pwd = forms.CharField(min_length=4,max_length=20, required=False)
+    pwd = forms.CharField(label="Mot de passe",widget=forms.PasswordInput(attrs={'class':' input form-control','placeholder':'Mot de passe'}),min_length=4,max_length=20, required=True)
--- app_auth/views.py
+++ app_auth/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate
 from django.contrib import messages
+from .forms import LoginForm
 
 # Create your views here.
 
 def login_blog(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        pwd = request.POST["pwd"]
-        user = authenticate(username=username, password=pwd)
-        if user is not None:
-            return redirect("home")
+   if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            pwd = form.cleaned_data['pwd']
+            user = authenticate(username=username, password=pwd)
+            if user is not None:
+                return redirect("home")
             # return redirect("Sign-up.html Apropos")
+            else:
+                messages.error(request,"Erreur d'authentification")
+                return render(request,"login.html",{"form":form})
         else:
-            messages.error(request,"Erreur d'authentification")
-            return render(request,"login.html")
-            # message.error(request,"echec de la connexion")
-        
-        
-    return render(request,"login.html")
+            return render(request,"login.html",{"form":form})
+
+   else:
+        form = LoginForm()
+        return render(request,"login.html",{"form":form})
 
 
 def signup_blog(request):
