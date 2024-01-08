34baf2c55cc4443ba3f322784ba84a9482ac298a
stevy-kaba <stevyaimery@gmail.com>
2024-01-08 13:36:06 +0100

    adding app-admin v2 good

--- app_admin/views.py
+++ app_admin/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from my_blog.models import Produit
 from django.contrib.auth.decorators import login_required
 
@@ -6,7 +6,8 @@ from django.contrib.auth.decorators import login_required
 def dashboard(request):
     return render(request,'db.html')
 
-# @login_required
 def user_articles(request):
+    if not request.user.is_authenticated:
+        return redirect('home')
     list_produits = Produit.objects.filter(user=request.user)
     return render(request, 'my-produits.html', {'liste_produits': list_produits})
